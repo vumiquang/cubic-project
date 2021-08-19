ba4d21d9585e973ca6bb74430bfccdc81c6542f8
vumiquang <vuminhquang.dsn@gmail.com>
2021-08-19 16:49:24 +0700

    create project

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home</title>
+    <link
+      rel="shortcut icon"
+      href="./assets/images/cubicfavicon.png"
+      type="image/x-icon"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Nunito:200,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=vietnamese"
+      rel="stylesheet"
+    />
+    <script src="./assets/js/font-awesome.min.js"></script>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="./css/reset.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+  <body>
+    <header>
+      <nav class="nav">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Project</a>
+            <ul class="sub-menu">
+              <li><span>Lorem, ipsum.</span></li>
+              <li><a href="#">Lorem.</a></li>
+              <li><a href="#">Lorem</a></li>
+              <li><a href="#">Lorem</a></li>
+              <li><a href="#">Lorem</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">About</a>
+            <ul class="sub-menu">
+              <li><span>Lorem, ipsum.</span></li>
+              <li><a href="#">Lorem.</a></li>
+              <li><a href="#">Lorem</a></li>
+              <li><a href="#">Lorem</a></li>
+              <li><a href="#">Lorem</a></li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">News</a>
+            <ul class="sub-menu">
+              <li><span>Lorem, ipsum.</span></li>
+              <li><a href="#">Lorem.</a></li>
+              <li><a href="#">Lorem</a></li>
+              <li><a href="#">Lorem</a></li>
+              <li><a href="#">Lorem</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Contact us</a></li>
+        </ul>
+        <div class="language d-flex">
+          <a href="#"><img src="./assets/images/vietnam.png" alt="VN" /></a>
+          <a href="#"
+            ><img src="./assets/images/united-kingdom.png" alt="US"
+          /></a>
+        </div>
+      </nav>
+      <div class="carousel-wrapper">
+        <div
+          id="carouselExampleControls"
+          class="carousel slide"
+          data-ride="carousel"
+        >
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                class="d-block w-100"
+                src="./assets/images/banner/img1.jpeg"
+                alt="First slide"
+              />
+              <a href="#">Dragon palace</a>
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-100"
+                src="./assets/images/banner/img1.jpeg"
+                alt="Second slide"
+              />
+              <a href="#">Dragon palace</a>
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-100"
+                src="./assets/images/banner/img1.jpeg"
+                alt="Third slide"
+              />
+              <a href="#">Dragon palace</a>
+            </div>
+          </div>
+          <a
+            class="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <i
+              class="fas fa-caret-square-left icon-prev"
+              aria-hidden="true"
+            ></i>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <i
+              aria-hidden="true"
+              class="fas fa-caret-square-right icon-next"
+            ></i>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <img
+          src="./assets/images/cubic_logo_white_green-p-500x285.png"
+          alt="logo"
+        />
+      </div>
+    </header>
+
+    <section class="news">
+      <div class="title"></div>
+    </section>
+    <script src="./assets/js/jquery-3.2.1.slim.min.js"></script>
+    <script src="./assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
